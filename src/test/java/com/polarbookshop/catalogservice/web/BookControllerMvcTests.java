6ad5ccd307e005dc1c0df8354bb215770560c84f
stk346 <stk346@gmail.com>
2024-09-04 01:02:52 +0900

    feat: ch3.4.3 @WebMvcTest를 사용한 REST 컨트롤러의 테스트

---
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,33 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbb = "73737317940";
+        BDDMockito.given(bookService.viewBookDetails(isbb))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbb))
+                .andExpect(status().isNotFound());
+    }
+}
