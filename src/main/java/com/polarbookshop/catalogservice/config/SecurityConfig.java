eab52b314405e960d36e320e97883c5da1d07d08
stk346 <stk346@gmail.com>
2024-09-18 05:49:31 +0900

    feat: ch12.2.1 스프링 부트 OAuth2 리소스 서버 보호

---
+++ src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
+                        .anyRequest().authenticated() // 이 외의 다른 요청은 인증이 필요하다.
+                )
+                // JWT에 기반한 기본 설정을 사용해 OAuth2 리소스 서버 지원을 활성화
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                // 각 요청은 액세스 토큰을 가지고 있어야만 함. (상태를 갖지 않기를 원함)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 인증 전략이 상태를 갖지 않고 브라우저 기반 클라이언트가 관여되지 않기 때문에 CSRF 보호를 비활성화해도 됨.
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
